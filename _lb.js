0ca135b3f4ed6a9ba5a8dcfdf255119a487cd6cd
Zhuanxi <112997721+TabawaX@users.noreply.github.com>
2025-01-12 22:48:03 +0700

    Add files via upload

---
+++ _lb.js
@@ -0,0 +1,8 @@
+[
+    {
+        "name": "Elaina",
+        "anime": "Majo no tabi tabj",
+        "rank": 1,
+        "thumb": "https://raw.githubusercontent.com/TabawaX/waifudb/refs/heads/master/docs/forum/IMG-20250112-WA0158.jpg"
+    }
+]
