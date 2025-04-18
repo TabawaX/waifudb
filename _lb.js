b30cbdcef3cafeaaba323d9c2677a19fb902ef67
Zhuanxi <112997721+TabawaX@users.noreply.github.com>
2025-04-18 12:14:50 +0700

    Add files via upload

--- _lb.js
+++ _lb.js
@@ -4,5 +4,11 @@
         "anime": "Majo no Tabitabi",
         "rank": 1,
         "thumb": "https://raw.githubusercontent.com/TabawaX/waifudb/refs/heads/master/docs/forum/%231Elaina.jpg"
+    },
+    {
+        "name": "Furina",
+        "anime": "Game: Genshin Impact",
+        "rank": 1,
+        "thumb": "https://raw.githubusercontent.com/TabawaX/waifudb/refs/heads/master/docs/forum/20250418_120341.jpg"
     }
 ]
